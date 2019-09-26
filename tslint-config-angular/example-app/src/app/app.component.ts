17929f6f42fbdf6a2ac135185a6887a1a3fedf89
Filip Voska <filip.voska@infinum.hr>
2019-09-26 09:58:45 +0200

    Add some sample code for linter checks

--- tslint-config-angular/example-app/src/app/app.component.ts
+++ tslint-config-angular/example-app/src/app/app.component.ts
@@ -1,10 +1,9 @@
 import { Component } from '@angular/core';
+import { Subject } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'infinum-tslint-config-angular-example-app';
-}
+export class AppComponent { }
--- tslint-config-angular/example-app/src/app/app.module.ts
+++ tslint-config-angular/example-app/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { WrongSelectorComponent } from './components/wrong-selector/wrong-selector.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WrongSelectorComponent
   ],
   imports: [
     BrowserModule,
