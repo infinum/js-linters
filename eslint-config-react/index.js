dfcee47126f685aeae782c58d00f175d6e165a31
Ivica Batinic <ivica.batinic@infinum.hr>
2020-03-17 09:50:37 +0100

    Update eslint react

--- eslint-config-react/index.js
+++ eslint-config-react/index.js
@@ -1,53 +1,28 @@
 module.exports = {
-  plugins: [
-    'react',
-    'jsx-a11y',
-    'emotion',
+  extends: [
+    'plugin:react/recommended',
+    'plugin:jsx-a11y/recommended',
+    '@infinumjs',
+    'prettier/react'
   ],
-  extends: ['@infinumjs', 'plugin:jsx-a11y/recommended', 'plugin:react/all'],
-  rules: {
-
-    // overridden react rules
-    'react/jsx-max-props-per-line': [1, {
-      maximum: 1,
-    }],
-    'react/jsx-no-bind': [1, {
-      ignoreRefs: false,
-      allowArrowFunctions: false,
-      allowBind: false,
-    }],
-    'react/jsx-filename-extension': [1, {
-      extensions: ['.js', '.jsx'],
-    }],
-    'react/prefer-stateless-function': 1,
-    'react/prop-types': 1,
-    'react/jsx-indent': [2, 2],
-    'react/jsx-indent-props': [2, 2],
-    'react/jsx-no-literals': 0,
-    'react/no-set-state': 1,
-
-    // overridden import rules
-    'import/prefer-default-export': 0,
-    'import/extensions': 0,
-    'import/no-extraneous-dependencies': [2, {
-      devDependencies: true,
-    }],
-
-    'no-unused-vars': 1,
-    'class-methods-use-this': [1, {
-      exceptMethods: [
-        'render',
-        'constructor',
-        'componentWillMount',
-        'componentDidMount',
-        'componentWillReceiveProps',
-        'shouldComponentUpdate',
-        'componentWillUpdate',
-        'componentDidUpdate',
-        'componentWillUnmount',
-        'componentDidCatch',
-      ],
-    }],
-    'object-curly-spacing': 0,
+  plugins: ['react-hooks', 'jsx-a11y'],
+  settings: {
+    react: {
+      version: 'detect'
+    },
   },
+  rules: {
+    'react/react-in-jsx-scope': 0,
+    'react/prop-types': [
+      'error',
+      {
+        'ignore': [],
+        'customValidators': [],
+        'skipUndeclared': true
+      }
+    ],
+    'jsx-a11y/anchor-is-valid': 'warn',
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn'
+  }
 };
