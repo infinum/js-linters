eff5e044ea0eab1f0d2d818112aae46f484915d0
Miroslav Foltyn <miroslav.foltyn64@gmail.com>
2021-07-02 13:07:28 +0200

    feat(config): added example-app for eslint-config-angular-ts

---
+++ packages/eslint-config-angular-ts/example-app/src/app/app.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'my-app';
+
+  // example linter error follow
+  obs: Observable<void>;
+
+  // example linter error follow
+  ngOnInit(): {}
+}
