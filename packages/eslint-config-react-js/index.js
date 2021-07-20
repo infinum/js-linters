b0b96b8c1079908ca02422a114114f11b2481a26
Ivica Batinić <isBatak@users.noreply.github.com>
2021-07-20 12:04:07 +0200

    Update react-hooks/exhaustive-deps rule to error
    Our team is constantly ignoring this rule all the time, so I think we should be more strict with this rule.

--- packages/eslint-config-react-js/index.js
+++ packages/eslint-config-react-js/index.js
@@ -23,7 +23,7 @@ module.exports = {
     ],
     'jsx-a11y/anchor-is-valid': 'warn',
     'react-hooks/rules-of-hooks': 'error',
-    'react-hooks/exhaustive-deps': 'warn',
+    'react-hooks/exhaustive-deps': 'error',
     'no-param-reassign': 'warn',
     'consistent-return': 'warn',
   },
