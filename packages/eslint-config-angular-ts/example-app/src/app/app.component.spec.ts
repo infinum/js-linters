bd5f1453b3ad5917728fb015fc0842fecaf92b8e
Kristijan Bambir <kristijan.bambir@infinum.com>
2021-10-25 08:56:51 +0200

    Update example repo

--- packages/eslint-config-angular-ts/example-app/src/app/app.component.spec.ts
+++ packages/eslint-config-angular-ts/example-app/src/app/app.component.spec.ts
@@ -1,13 +1,9 @@
 import { TestBed } from '@angular/core/testing';
-import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [
-        RouterTestingModule
-      ],
       declarations: [
         AppComponent
       ],
@@ -20,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'my-app'`, () => {
+  it(`should have as title 'example-app'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('my-app');
+    expect(app.title).toEqual('example-app');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
-    const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('my-app app is running!');
+    const compiled = fixture.nativeElement as HTMLElement;
+    expect(compiled.querySelector('.content span')?.textContent).toContain('example-app app is running!');
   });
 });
--- packages/eslint-config-angular-ts/example-app/src/app/app.component.ts
+++ packages/eslint-config-angular-ts/example-app/src/app/app.component.ts
@@ -1,6 +1,4 @@
 import { Component } from '@angular/core';
-import { Router } from '@angular/router';
-import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -8,17 +6,5 @@ import { Observable } from 'rxjs';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  // example linter error follows
-  title = 'my-app';
-
-  // example linter error follow
-  obs: Observable<void>;
-
-  constructor(
-    // example linter error follow
-    private router: Router,
-  ) {}
-
-  // example linter error follow
-  ngOnInit(): {}
+  title = 'example-app';
 }
