9682817847edfb12aafbaa4ad4d16e01d2118e16
Kristijan Bambir <kristijan.bambir@infinum.com>
2021-11-08 15:02:01 +0100

    Add no-nested-ternary

--- packages/eslint-config-core-js/index.js
+++ packages/eslint-config-core-js/index.js
@@ -1,20 +1,21 @@
 module.exports = {
-  root: true,
-  env: {
-    browser: true,
-    commonjs: true,
-    es6: true,
-    es2017: true,
-    es2020: true,
-    node: true,
-  },
-  extends: ['eslint:recommended'],
-  parserOptions: {
-    ecmaVersion: 2018,
-    sourceType: 'module',
-  },
-  rules: {
-    semi: 'error',
-    'no-implicit-coercion': 'error',
-  },
+	root: true,
+	env: {
+		browser: true,
+		commonjs: true,
+		es6: true,
+		es2017: true,
+		es2020: true,
+		node: true,
+	},
+	extends: ['eslint:recommended'],
+	parserOptions: {
+		ecmaVersion: 2018,
+		sourceType: 'module',
+	},
+	rules: {
+		semi: 'error',
+		'no-implicit-coercion': 'error',
+		'no-nested-ternary': 'error',
+	},
 };
