56231c8501e34933c5b9cbab3d8b83528690779e
Ivica Batinic <ivica.batinic@infinum.hr>
2021-11-26 15:06:15 +0100

    Init snapshot test

---
+++ examples/nextjs/test/eslint.test.ts
@@ -0,0 +1,15 @@
+import { ESLint } from 'eslint';
+
+it('ESLint settings regression test', async () => {
+	const eslint = new ESLint({ fix: false });
+	const results = await eslint.lintFiles(['**/*.ts', '**/*.tsx']);
+
+	await ESLint.outputFixes(results);
+
+	const formatter = await eslint.loadFormatter('visualstudio');
+	const resultText = formatter.format(results);
+
+	console.log(resultText);
+
+	(expect(resultText) as any).toMatchSnapshot();
+});
