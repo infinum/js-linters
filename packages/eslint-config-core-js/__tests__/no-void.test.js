55d130fd3a34e61d611ef3cba5c69606b0b59b75
Gordan Bareza <gordan.bareza@infinum.hr>
2022-07-07 20:27:15 +0200

    no-void test

---
+++ packages/eslint-config-core-js/__tests__/no-void.test.js
@@ -0,0 +1,28 @@
+import { test } from 'uvu';
+import { getTester } from '@infinumjs/test-utils';
+
+import eslintConfig from '../index';
+
+const tester = getTester({
+	filePath: __filename,
+	eslintConfig: eslintConfig,
+	rule: 'no-void',
+});
+
+test('disallow void operators 1', () => tester.invalid(`void foo;`, [`Expected 'undefined' and instead saw 'void'.`]));
+
+test('disallow void operators 2', () =>
+	tester.invalid(`void someFunction();`, [`Expected 'undefined' and instead saw 'void'.`]));
+
+test('disallow void operators 3', () =>
+	tester.invalid(`var foo = void bar();`, [`Expected 'undefined' and instead saw 'void'.`]));
+
+test('disallow void operators 4', () =>
+	tester.invalid(
+		`function baz() {
+			return void 0;
+		}`,
+		[`Expected 'undefined' and instead saw 'void'.`]
+	));
+
+test.run();
