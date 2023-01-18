94ff9593a226110677b09290c7bdb5fda669c406
Gordan Bareza <gordan.bareza@infinum.hr>
2023-01-18 10:16:18 +0100

    Add linter warnings for using TS enums

--- packages/eslint-config-react-ts/index.js
+++ packages/eslint-config-react-ts/index.js
@@ -1,6 +1,15 @@
 module.exports = {
-	extends: ['@infinumjs/eslint-config-core-ts', '@infinumjs/eslint-config-react-js'],
+	extends: [
+		'@infinumjs/eslint-config-core-ts',
+		'@infinumjs/eslint-config-react-js',
+		'plugin:typescript-enum/recommended',
+	],
+	plugins: ['typescript-enum'],
 	globals: {
 		JSX: true,
+	},
+	rules: {
+		'typescript-enum/no-enum': 'warn',
+		'typescript-enum/no-const-enum': 'warn',
 	},
 };
