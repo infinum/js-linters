1089022c93105a4011077d506f64c44ca38f1c68
Kristian Djaković <kristian.djakovic@infinum.hr>
2023-04-03 10:18:59 +0200

    Remove comments

--- packages/eslint-plugin-nextjs-ts/index.js
+++ packages/eslint-plugin-nextjs-ts/index.js
@@ -7,10 +7,7 @@ module.exports = {
 		recommended: {
 			plugins: ['@infinumjs/nextjs-ts'],
 			rules: {
-				// '@infinumjs/no-hooks-in-pages-folder': 'error',
-				// 'no-hooks-in-pages-folder': 'error',
 				'@infinumjs/nextjs-ts/no-hooks-in-pages-folder': 'error',
-				// '@infinumjs/eslint-plugin-nextjs-ts/no-hooks-in-pages-folder': 'error',
 			},
 		},
 	},
