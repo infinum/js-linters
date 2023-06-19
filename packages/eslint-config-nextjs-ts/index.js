f3e8ade04c8fde33479a2a6dbf69794fdde4153f
Gordan Bareza <48893276+gbareza@users.noreply.github.com>
2023-06-19 09:29:47 +0200

    __NEXT_DATA__ tests (#109)
    * __NEXT_DATA__ tests
    
    * Add another use example

--- packages/eslint-config-nextjs-ts/index.js
+++ packages/eslint-config-nextjs-ts/index.js
@@ -1,5 +1,11 @@
 module.exports = {
 	extends: ['plugin:@next/next/recommended', '@infinumjs/eslint-config-react-ts'],
+	parserOptions: {
+		tsconfigRootDir: __dirname,
+		project: ['__tests__/tsconfig.json'],
+		ecmaVersion: 2018,
+		sourceType: 'module',
+	},
 	rules: {
 		'no-underscore-dangle': ['error', { allow: ['__NEXT_DATA__'] }],
 	},
