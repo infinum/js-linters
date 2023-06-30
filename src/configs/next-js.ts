b2b1bf06aa238bb95bcf6b49a641930319ba5dc6
Kristian Djaković <kristian.djakovic@infinum.hr>
2023-06-30 15:44:47 +0200

    fix: remove no-hooks-in-pages-folder

--- src/configs/next-js.ts
+++ src/configs/next-js.ts
@@ -2,9 +2,7 @@ import { TSESLint } from '@typescript-eslint/utils';
 
 export default {
 	extends: ['plugin:@next/next/recommended'],
-	plugins: ['@infinum'],
 	rules: {
-		'@infinum/no-hooks-in-pages-folder': 'error',
 		'no-underscore-dangle': ['error', { allow: ['__NEXT_DATA__'] }],
 	},
 } satisfies TSESLint.Linter.Config;
