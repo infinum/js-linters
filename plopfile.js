22eb9599a73b7be1f0a1f24ed4315121f9d68c32
Kristian Djaković <kristian.djakovic@infinum.hr>
2023-06-30 09:53:20 +0200

    chore: add rule generator

--- plopfile.js
+++ plopfile.js
@@ -32,8 +32,42 @@ module.exports = function main(plop) {
 
 					const config = await prettier.resolveConfig();
 
+					const newFile = prettier.format([importStatement, file.replace(/(\n\};)/m, `\n${name},$1`)].join(''), config);
+
+					return newFile;
+				},
+			},
+		],
+	});
+
+	plop.setGenerator('rule', {
+		description: 'Generates a new eslint rule',
+		prompts: [
+			{
+				type: 'input',
+				name: 'name',
+				message: 'Enter the name of the rule',
+			},
+			{
+				type: 'input',
+				name: 'description',
+				message: 'Enter the description of the rule',
+			},
+		],
+		actions: [
+			{
+				type: 'add',
+				path: 'src/rules/{{kebabCase name}}.ts',
+				templateFile: 'plop/rule/rule.hbs',
+			},
+			{
+				type: 'modify',
+				path: 'src/rules/index.ts',
+				transform: async (file, { name }) => {
+					const config = await prettier.resolveConfig();
+
 					const newFile = prettier.format(
-						[importStatement, file.replace(/(\n\};)/m, `\n\t${name},$1`)].join(''),
+						file.replace(/\{(.*)\}/m, `{$1\n${name}: require('./rules/${name}')}`),
 						config
 					);
 
